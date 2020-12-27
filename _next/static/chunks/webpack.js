a2611717d3861568033a8336790cc7cba381925e
Mostafa Amininasab <m.amininasab@alibaba.ir>
2020-12-27 23:32:17 +0330

    update

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "79ae585d3136da3faaa8";
+/******/ 	var hotCurrentHash = "505d02ab3fd52eb44e06";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
